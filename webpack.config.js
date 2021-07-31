ca63500a346b9a428c20fdc8f5b225169ae1538c
Umer Hurramov <umer.hurramov@gmail.com>
2021-07-31 13:01:37 +0300

    first commit

---
+++ webpack.config.js
@@ -0,0 +1,62 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const CopyPlugin = require('copy-webpack-plugin')
+
+module.exports = {
+  entry: './src/index.ts',
+  devtool: 'inline-source-map',
+  mode: 'development',
+  module: {
+    rules: [
+      {
+        test: /\.tsx?$/,
+        use: 'ts-loader',
+        exclude: /node_modules/,
+      },
+      {
+        test: /\.s[ac]ss$/i,
+        use: [
+          // Creates `style` nodes from JS strings
+          "style-loader",
+          // Translates CSS into CommonJS
+          "css-loader",
+          // Compiles Sass to CSS
+          {
+            loader: "sass-loader",
+            options: {
+              sourceMap: true,
+              // sassOptions: {
+              //   outputStyle: "compressed",
+              // },
+            },
+          },
+        ],
+      },
+    ],
+  },
+  resolve: {
+    extensions: ['.tsx', '.ts', '.js'],
+  },
+  output: {
+    filename: 'bundle.js',
+    path: path.resolve(__dirname, 'dist'),
+  },
+  devServer: {
+    contentBase: path.join(__dirname, 'dist'),
+    compress: false,
+    port: 8080,
+    liveReload: true,
+    open: true,
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: path.relative(__dirname, './public/index.html'),
+      title: 'Diyar oyun'
+    }),
+    new CopyPlugin({
+      patterns: [
+        { from: path.resolve(__dirname, 'public/assets'), to: path.resolve(__dirname, 'dist/assets') }
+      ],
+    }),
+  ],
+};
