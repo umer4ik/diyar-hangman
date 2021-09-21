f22ef6a135fb1495dc49e468214e6bbe5b6d9233
Umer Hurramov <umer.hurramov@gmail.com>
2021-09-21 20:51:22 +0300

    ecmascript way

--- webpack.config.js
+++ webpack.config.js
@@ -1,52 +1,59 @@
-const path = require('path');
-const HtmlWebpackPlugin = require('html-webpack-plugin');
+const path = require('path')
+const HtmlWebpackPlugin = require('html-webpack-plugin')
 const CopyPlugin = require('copy-webpack-plugin')
 
 module.exports = {
-  entry: './src/index.ts',
+  entry: './src/index.js',
   devtool: 'inline-source-map',
   mode: 'development',
   module: {
     rules: [
       {
-        test: /\.tsx?$/,
-        use: 'ts-loader',
-        exclude: /node_modules/,
+        test: /\.jsx?$/,
+        use: [
+          {
+            loader: 'babel-loader',
+            options: {
+              presets: ['@babel/preset-env']
+            }
+          }
+        ],
+        exclude: /node_modules/
       },
       {
         test: /\.s[ac]ss$/i,
         use: [
           // Creates `style` nodes from JS strings
-          "style-loader",
+          'style-loader',
           // Translates CSS into CommonJS
-          "css-loader",
+          'css-loader',
           // Compiles Sass to CSS
           {
-            loader: "sass-loader",
+            loader: 'sass-loader',
             options: {
-              sourceMap: true,
+              sourceMap: true
               // sassOptions: {
               //   outputStyle: "compressed",
               // },
-            },
-          },
-        ],
-      },
-    ],
+            }
+          }
+        ]
+      }
+    ]
   },
   resolve: {
-    extensions: ['.tsx', '.ts', '.js'],
+    extensions: ['.tsx', '.ts', '.js']
   },
   output: {
     filename: 'bundle.js',
-    path: path.resolve(__dirname, 'dist'),
+    path: path.resolve(__dirname, 'dist')
   },
   devServer: {
     contentBase: path.join(__dirname, 'dist'),
     compress: false,
     port: 8080,
     liveReload: true,
-    open: true,
+    open: true
   },
   plugins: [
     new HtmlWebpackPlugin({
@@ -56,7 +63,7 @@ module.exports = {
     new CopyPlugin({
       patterns: [
         { from: path.resolve(__dirname, 'public/assets'), to: path.resolve(__dirname, 'dist/assets') }
-      ],
-    }),
-  ],
-};
+      ]
+    })
+  ]
+}
